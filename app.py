26e1f7d2ac8a7b6a6029b121f9c0681295d57022
Mohammad Reza <mohammadreza.rahmani89@gmail.com>
2019-08-03 06:02:01 +0430

    Hashing Users

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
 
 import models
+import config
 from resources.courses import courses_api
 from resources.reviews import reviews_api
-
-DEBUG = True
-HOST = '0.0.0.0'
-PORT = 5000
 
 app = Flask(__name__)
 app.register_blueprint(courses_api)
@@ -20,4 +17,4 @@ def hello_world():
 
 if __name__ == '__main__':
 	models.initialize()
-	app.run(debug=DEBUG, host=HOST, port=PORT)
+	app.run(debug=config.DEBUG, host=config.HOST, port=config.PORT)
--- models.py
+++ models.py
@@ -1,8 +1,19 @@
 import datetime
 
 from peewee import *
+from argon2 import PasswordHasher
 
 DATABASE = SqliteDatabase('db.sqlite3')
+HASHER = PasswordHasher
+
+
+class User(Model):
+	username = CharField(unique=True)
+	email = CharField(unique=True)
+	password = CharField()
+
+	class Meta:
+		database = DATABASE
 
 
 class Course(Model):
@@ -12,6 +23,26 @@ class Course(Model):
 
 	class Meta:
 		database = DATABASE
+
+	@classmethod
+	def create_user(cls, username, email, password, **kwargs):
+		email = email.lower()
+		try:
+			cls.select().where((cls.email == email) | (cls.username ** username)).get()
+		except cls.DoesNotExist:
+			user = cls(username=username, email=email)
+			user.password = user.set_password(password)
+			user.save()
+			return user
+		else:
+			raise Exception("User with that email or username already exists")
+
+	@staticmethod
+	def set_password(password):
+		return HASHER.hash(password)
+
+	def verify_password(self, password):
+		return HASHER.verify(self.password, password)
 
 
 class Review(Model):
